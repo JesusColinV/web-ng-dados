02b8524d20ee60900e950639186b931b11d9717a
JesusColinV <cev3107@innovaccion.mx>
2021-10-21 08:20:51 -0500

    middle of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-ng-dados';
+  diceLeft="../assets/img/dice1.png"
+  diceRight="../assets/img/dice2.png"
+  number1: number;
+  number2: number;
+
+  constructor(){
+    this.number1=Math.random();
+    this.number2=Math.random();
+  }
+
+  throwDice(): void{
+    this.number1=Math.round(Math.random()*5)+1; 
+    this.number2=Math.round(Math.random()*5)+1; 
+    console.log(this.number1);
+    console.log(this.number2);
+  }
 }
