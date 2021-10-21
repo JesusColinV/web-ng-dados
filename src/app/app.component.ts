a128870ebb41dd5e13d641cff776bb6bf4b654dd
JesusColinV <cev3107@innovaccion.mx>
2021-10-21 08:27:40 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,20 @@ export class AppComponent {
   number2: number;
 
   constructor(){
-    this.number1=Math.random();
-    this.number2=Math.random();
+    this.number1=Math.round(Math.random()*5)+1; 
+    this.number2=Math.round(Math.random()*5)+1;
+    this.diceLeft="../assets/img/dice"+this.number1+".png"
+    this.diceRight="../assets/img/dice"+this.number2+".png"
   }
 
   throwDice(): void{
     this.number1=Math.round(Math.random()*5)+1; 
     this.number2=Math.round(Math.random()*5)+1; 
-    console.log(this.number1);
-    console.log(this.number2);
+    //console.log(this.number1);
+    //console.log(this.number2);
+
+    this.diceLeft="../assets/img/dice"+this.number1+".png"
+    this.diceRight="../assets/img/dice"+this.number2+".png"
   }
+
 }
